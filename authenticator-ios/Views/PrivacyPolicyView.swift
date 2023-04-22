0be43580e74e13876f932c9c80c8aa3ee3096cf8
Kel <codesmithk@gmail.com>
2023-04-22 13:32:54 -0400

    Added privacy policy view

---
+++ authenticator-ios/Views/PrivacyPolicyView.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacyPolicyView.swift
+//  authenticator-ios
+//
+//  Created by Kelvin Reid on 4/19/23.
+//
+
+import SwiftUI
+
+struct PrivacyPolicyView: View {
+    var body: some View {
+        NavigationView {
+            Text("[Add Privacy Policy]")
+                .navigationBarTitle(Text("Privacy Policy"), displayMode: .large)
+        }
+    }
+}
