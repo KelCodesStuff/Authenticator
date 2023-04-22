fc6e07fb9e8d06a7f71d5fd435c78b0e1204a10a
Kel <codesmithk@gmail.com>
2023-04-22 16:00:47 -0400

    Added passwords view

---
+++ authenticator-ios/Views/PasswordsView.swift
@@ -0,0 +1,18 @@
+//
+//  PasswordsView.swift
+//  authenticator-ios
+//
+//  Created by Kelvin Reid on 4/22/23.
+//
+
+import SwiftUI
+
+struct PasswordsView: View {
+    
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+   
+}
