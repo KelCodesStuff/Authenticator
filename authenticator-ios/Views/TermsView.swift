6c363906ea2a089f647182f6fa9a06ee82467d44
Kel <codesmithk@gmail.com>
2023-04-22 13:33:33 -0400

    Added terms view

---
+++ authenticator-ios/Views/TermsView.swift
@@ -0,0 +1,17 @@
+//
+//  TermsView.swift
+//  authenticator-ios
+//
+//  Created by Kelvin Reid on 4/19/23.
+//
+
+import SwiftUI
+
+struct TermsView: View {
+    var body: some View {
+        NavigationView {
+            Text("[Add Terms of Use]")
+                .navigationBarTitle(Text("Terms of Use"), displayMode: .large)
+        }
+    }
+}
