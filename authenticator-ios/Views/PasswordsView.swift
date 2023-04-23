eaa5e622061c1097dcf22dcd1470a00b96fcfa27
Kel <codesmithk@gmail.com>
2023-04-22 20:10:26 -0400

    Updated passwords view

--- authenticator-ios/Views/PasswordsView.swift
+++ authenticator-ios/Views/PasswordsView.swift
@@ -8,11 +8,97 @@
 import SwiftUI
 
 struct PasswordsView: View {
+    @State var password = ""
+    @State var passwordLength = 8
+    @State var includeSpecialCharacters = true
+    @State var includeNumbers = true
     
     var body: some View {
         NavigationView {
-            
+            VStack {
+                Spacer()
+                Text("Password:")
+                    .font(.headline)
+                    .padding(.bottom, 10)
+                
+                Text(password)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .border(Color.gray)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(4)
+                    .padding(.horizontal)
+                    
+                
+                Button(action: {
+                    UIPasteboard.general.string = password
+                }) {
+                    Text("Copy Password")
+                        .font(.footnote)
+                        .padding()
+                        .foregroundColor(.blue)
+                        .cornerRadius(10)
+                }
+                .padding(.vertical, 10)
+
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Text("Password Length: \(passwordLength)")
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 16)
+                        
+                    Spacer()
+                    Stepper(
+                        value: $passwordLength,
+                        in: 8...32,
+                        label: {
+                            EmptyView()
+                        }
+                    )
+                }
+                
+                Toggle("Include Special Characters", isOn: $includeSpecialCharacters)
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 25)
+                
+                Toggle("Include Numbers", isOn: $includeNumbers)
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 25)
+                
+                Button(action: {
+                    password = generatePassword()
+                }) {
+                    Text("New Password")
+                        .font(.footnote)
+                        .padding()
+                        .foregroundColor(.blue)
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
+            }
         }
     }
-   
+    
+    func generatePassword() -> String {
+        var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        if includeSpecialCharacters {
+            letters += "!@#$%^&*()_-+="
+        }
+        if includeNumbers {
+            letters += "0123456789"
+        }
+        return String((0..<passwordLength).compactMap { _ in letters.randomElement() })
+
+    }
+}
+
+struct Password_Previews: PreviewProvider {
+    static var previews: some View {
+        PasswordsView()
+    }
 }
