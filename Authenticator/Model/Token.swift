043af71ddccad8f4be360bc0b9134b6e10253868
Kelvin Reid <codesmithk@gmail.com>
2023-12-12 22:17:07 -0500

    Updating model files

--- Authenticator/Model/Token.swift
+++ Authenticator/Model/Token.swift
@@ -1,3 +1,10 @@
+//
+//  Token.swift
+//  Authenticator
+//
+//  Created by Kel Reid on 06/29/23
+//
+
 import Foundation
 
 struct Token: Hashable, Identifiable {
@@ -35,39 +42,39 @@ struct Token: Hashable, Identifiable {
                 }
                 
                 if let algorithmParameter: URLQueryItem = queryItems.filter( { $0.name.lowercased() == "algorithm" }).first {
-                        switch (algorithmParameter.value ?? "SHA1").uppercased() {
-                        case "SHA1":
-                                self.algorithm = .sha1
-                        case "SHA256":
-                                self.algorithm = .sha256
-                        case "SHA512":
-                                self.algorithm = .sha512
-                        default:
-                                self.algorithm = .sha1
+                    switch (algorithmParameter.value ?? "SHA1").uppercased() {
+                    case "SHA1":
+                            self.algorithm = .sha1
+                    case "SHA256":
+                            self.algorithm = .sha256
+                    case "SHA512":
+                            self.algorithm = .sha512
+                    default:
+                            self.algorithm = .sha1
                         }
                 } else {
                         self.algorithm = .sha1
                 }
                 
                 if let digitsParameter: URLQueryItem = queryItems.filter( { $0.name.lowercased() == "digits" }).first {
-                        switch digitsParameter.value ?? "6" {
-                        case "7":
-                                self.digits = 7
-                        case "8":
-                                self.digits = 8
-                        default:
-                                self.digits = 6
+                    switch digitsParameter.value ?? "6" {
+                    case "7":
+                            self.digits = 7
+                    case "8":
+                            self.digits = 8
+                    default:
+                            self.digits = 6
                         }
                 } else {
                         self.digits = 6
                 }
                 
                 if let periodValue: String = queryItems.filter( { $0.name.lowercased() == "period" }).first?.value {
-                        if let periodNumber = Int(periodValue) {
-                                self.period = periodNumber > 0 ? periodNumber : 30
-                        } else {
-                                self.period = 30
-                        }
+                    if let periodNumber = Int(periodValue) {
+                            self.period = periodNumber > 0 ? periodNumber : 30
+                    } else {
+                            self.period = 30
+                    }
                 } else {
                         self.period = 30
                 }
@@ -79,17 +86,17 @@ struct Token: Hashable, Identifiable {
                 let pathcomponents: [String] = path.components(separatedBy: ":")
                 switch pathcomponents.count {
                 case 0:
-                        issuerPrefix = nil
-                        accountName = nil
+                    issuerPrefix = nil
+                    accountName = nil
                 case 1:
-                        issuerPrefix = nil
-                        accountName = pathcomponents[0]
+                    issuerPrefix = nil
+                    accountName = pathcomponents[0]
                 case 2:
-                        issuerPrefix = pathcomponents[0]
-                        accountName = pathcomponents[1]
+                    issuerPrefix = pathcomponents[0]
+                    accountName = pathcomponents[1]
                 default:
-                        issuerPrefix = nil
-                        accountName = nil
+                    issuerPrefix = nil
+                    accountName = nil
                 }
                 self.uri = uri
                 self.type = .totp
@@ -98,7 +105,7 @@ struct Token: Hashable, Identifiable {
                 self.id = self.secret + Date().timeIntervalSince1970.description
                 
                 if self.displayIssuer.isEmpty && self.issuer.hasContent {
-                        self.displayIssuer = issuer!
+                    self.displayIssuer = issuer!
                 }
         }
         
@@ -114,27 +121,27 @@ struct Token: Hashable, Identifiable {
                 guard OTPGenerator.totp(secret: secret) != nil else { return nil }
                 
                 let label: String = {
-                        if issuerPrefix.hasContent && accountName.hasContent {
-                                let text: String = "/" + issuerPrefix! + ":" + accountName!
-                                let path: String = text.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? .empty
-                                return path
-                        } else if issuerPrefix.hasContent && !accountName.hasContent {
-                                let text: String = "/" + issuerPrefix!
-                                let path: String = text.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? .empty
-                                return path
-                        } else if !issuerPrefix.hasContent && accountName.hasContent {
-                                let text: String = "/:" + accountName!
-                                let path: String = text.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? .empty
-                                return path
-                        } else {
-                                return .empty
-                        }
+                    if issuerPrefix.hasContent && accountName.hasContent {
+                        let text: String = "/" + issuerPrefix! + ":" + accountName!
+                        let path: String = text.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? .empty
+                        return path
+                    } else if issuerPrefix.hasContent && !accountName.hasContent {
+                        let text: String = "/" + issuerPrefix!
+                        let path: String = text.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? .empty
+                        return path
+                    } else if !issuerPrefix.hasContent && accountName.hasContent {
+                        let text: String = "/:" + accountName!
+                        let path: String = text.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? .empty
+                        return path
+                    } else {
+                        return .empty
+                    }
                 }()
                 let issuerParameter: String = {
-                        guard issuer.hasContent else { return .empty }
-                        let query = issuer!.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-                        guard query.hasContent else { return .empty }
-                        return "&issuer=\(query!)"
+                    guard issuer.hasContent else { return .empty }
+                    let query = issuer!.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                    guard query.hasContent else { return .empty }
+                    return "&issuer=\(query!)"
                 }()
                 let algorithmParameter: String = "&algorithm=\(algorithm)"
                 let digitsParameter: String = "&digits=\(digits)"
@@ -164,34 +171,34 @@ struct Token: Hashable, Identifiable {
         ///   - displayIssuer: TokenData displayIssuer
         ///   - displayAccountName: TokenData displayAccountName
         init?(id: String, uri: String, displayIssuer: String, displayAccountName: String) {
-                guard let temp: Token = Token(uri: uri) else { return nil }
-                self.id = id
-                self.uri = uri
-                self.type = temp.type
-                self.issuerPrefix = temp.issuerPrefix
-                self.accountName = temp.accountName
-                self.secret = temp.secret
-                self.issuer = temp.issuer
-                self.algorithm = temp.algorithm
-                self.digits = temp.digits
-                self.period = temp.period
-                self.displayIssuer = displayIssuer
-                self.displayAccountName = displayAccountName
+            guard let temp: Token = Token(uri: uri) else { return nil }
+            self.id = id
+            self.uri = uri
+            self.type = temp.type
+            self.issuerPrefix = temp.issuerPrefix
+            self.accountName = temp.accountName
+            self.secret = temp.secret
+            self.issuer = temp.issuer
+            self.algorithm = temp.algorithm
+            self.digits = temp.digits
+            self.period = temp.period
+            self.displayIssuer = displayIssuer
+            self.displayAccountName = displayAccountName
         }
         
         enum TokenType {
-                case totp
+            case totp
         }
 
         enum Algorithm: String {
-                case sha1 = "SHA1"
-                case sha256 = "SHA256"
-                case sha512 = "SHA512"
+            case sha1 = "SHA1"
+            case sha256 = "SHA256"
+            case sha512 = "SHA512"
         }
 }
 
 extension Token {
-        init() {
-                self.init(uri: "otpauth://totp/Error:null?algorithm=SHA1&digits=6&issuer=Error&period=30&secret=LLKRKYOT7UCHSHPR")!
-        }
+    init() {
+            self.init(uri: "otpauth://totp/Error:null?algorithm=SHA1&digits=6&issuer=Error&period=30&secret=LLKRKYOT7UCHSHPR")!
+    }
 }
