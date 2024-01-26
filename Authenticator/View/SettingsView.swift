d0701f3a305a8bdbaec90b09419996bf4a1e8a98
Kelvin Reid <codesmithk@gmail.com>
2024-01-26 17:15:07 -0500

    Update SettingsView.swift

--- Authenticator/View/SettingsView.swift
+++ Authenticator/View/SettingsView.swift
@@ -6,33 +6,81 @@
 //
 
 import SwiftUI
+import MobileCoreServices
+import UniformTypeIdentifiers
+import CloudKit
+
+
+
+let container = CKContainer.default()
+let privateDatabase = container.privateCloudDatabase
+
+class DocumentPickerDelegate: NSObject, ObservableObject, UIDocumentPickerDelegate {
+    @State private var isICloudBackupEnabled = false
+    
+    @Published var selectedURL: URL?
+
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let selectedURL = urls.first else { return }
+        self.selectedURL = selectedURL
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
 
 struct SettingsView: View {
     @Binding var isPresented: Bool
+    @StateObject private var documentPickerDelegate = DocumentPickerDelegate()
+    @State private var isICloudBackupEnabled = false
+    @State private var account = Account(username: "", password: "")
+
 
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("General"), footer: Text("")) {
-                    KeyValueRow("Version", value: Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "")
-                    Link(destination: URL(string: "https://github.com/kelcodesstuff/authenticator-iOS")!) {
-                        Label("Source Code", systemImage: "chevron.left.slash.chevron.right")
-                            .foregroundColor(Color.green)
-                    }
-                }
-                Section(header: Text("Passwords"), footer: Text("")) {
+                Section(header: Text("Actions"), footer: Text("")) {
                     Button(action: {
                         // Handle the import action from files
-                        // You can present a file picker or perform the necessary actions here
+                        self.importAction()
                     }) {
                         Label("Import", systemImage: "square.and.arrow.down")
                             .foregroundColor(Color.green)
                     }
                     Button(action: {
-                        // Handle the exmport action from files
-                        // You can present a file picker or perform the necessary actions here
+                        // Handle the export action to files
+                        self.exportAction()
                     }) {
                         Label("Export", systemImage: "square.and.arrow.up")
+                            .foregroundColor(Color.green)
+                    }
+                }
+                Section(header: Text("Backup"), footer: Text("")) {
+                    Toggle("iCloud Backup", isOn: $isICloudBackupEnabled)
+                            .toggleStyle(SwitchToggleStyle(tint: Color.green))
+                            .onChange(of: isICloudBackupEnabled) { newValue in
+                                // Handle the state change, for example, triggering iCloud backup
+                                if newValue {
+//                                    self.iCloudBackupEnabled(account: )
+                                } else {
+//                                    self.iCloudBackupDisabled(account: )
+                                }
+                            }
+                }
+                Section(header: Text("Information"), footer: Text("")) {
+                    KeyValueRow("Version", value: Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "")
+                    Link(destination: URL(string: "https://sites.google.com/view/onevrtech/privacy-policy")!) {
+                        Label("Privacy Policy", systemImage: "hand.raised")
+                            .foregroundColor(Color.green)
+                    }
+                    Link(destination: URL(string: "https://sites.google.com/view/onevrtech/terms-of-service")!) {
+                        Label("Terms of Service", systemImage: "note.text")
+                            .foregroundColor(Color.green)
+                    }
+                    Link(destination: URL(string: "https://sites.google.com/view/onevrtech/end-user-license-agreement")!) {
+                        Label("EULA", systemImage: "hand.thumbsup")
                             .foregroundColor(Color.green)
                     }
                 }
@@ -50,9 +98,37 @@ struct SettingsView: View {
         HStack {
             Text(key)
             Spacer()
-            Text(value)
+            Text(value ?? "N/A")
         }
     }
+    
+    // Function to enable iCloud backup
+    func iCloudBackupEnabled(account: Account) {
+        // Implement the logic to start iCloud backup if needed
+        // This might involve cancelling ongoing operations or removing backup-related data
+        print("Starting iCloud backup...")
+    }
+
+    // Function to disable iCloud backup
+    func iCloudBackupDisabled(account: Account) {
+        // Implement the logic to stop iCloud backup if needed
+        // This might involve cancelling ongoing operations or removing backup-related data
+        print("Stopping iCloud backup...")
+    }
+    
+    // Function to handle the import action
+    private func importAction() {
+        let documentPicker = UIDocumentPickerViewController(documentTypes: [String(kUTTypeCommaSeparatedText)], in: .import)
+        documentPicker.delegate = documentPickerDelegate
+        UIApplication.shared.windows.first?.rootViewController?.presentedViewController?.present(documentPicker, animated: true, completion: nil)
+    }
+
+    // Function to handle the export
+    private func exportAction() {
+        // Implement the logic for exporting data to a file
+        // You can present a file picker or perform the necessary actions here
+    }
+    
 }
 
 struct SettingsView_Previews: PreviewProvider {
