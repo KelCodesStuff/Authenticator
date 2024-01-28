d336c6adb2d23af2ccf54ca698af9796da5b26e4
KelCodesStuff <codesmithk@gmail.com>
2024-01-28 18:48:28 -0500

    Update PasswordsView.swift

--- Authenticator/View/Passwords/PasswordsView.swift
+++ Authenticator/View/Passwords/PasswordsView.swift
@@ -7,32 +7,55 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct PasswordsView: View {
     @State private var credentials: [Credential] = KeychainManager.shared.loadCredentials()
     @State private var showingDetail = false
+    @State private var isSheetPresented: Bool = false
+    @State private var editingCredential: Credential?
 
     var body: some View {
         NavigationView {
             List {
                 ForEach(credentials) { credential in
                     PasswordCardView(credential: credential)
+                        .onTapGesture {
+                            editingCredential = credential
+                            showingDetail = true
+                        }
                 }
                 .onDelete(perform: delete)
             }
             .navigationBarTitle("Passwords", displayMode: .inline)
-            .navigationBarItems(trailing: addButton)
-            .sheet(isPresented: $showingDetail) {
-                AddPasswordView(credentials: $credentials, isNewCredential: true)
+            .navigationBarItems(leading: settingsButton, trailing: addButton)
+            
+            .sheet(isPresented: $showingDetail, onDismiss: {
+                            editingCredential = nil // Reset the editing credential when the sheet is dismissed
+                        }) {
+                            AddPasswordView(credentials: $credentials, credentialToEdit: editingCredential)
+                        }
+            .sheet(isPresented: $isSheetPresented) {
+                switch presentingSheet {
+                case .showSettings:
+                    SettingsView(isPresented: $isSheetPresented)
+                }
             }
         }
-        .onAppear {
-            self.credentials = KeychainManager.shared.loadCredentials()
+    }
+    
+    private var settingsButton: some View {
+        Button(action: {
+            presentingSheet = .showSettings
+            isSheetPresented = true
+        }) {
+            Image(systemName: "gear")
         }
     }
-
+    
     private var addButton: some View {
         Button(action: {
+            editingCredential = nil // Reset the editing credential
             showingDetail = true
         }) {
             Image(systemName: "plus")
@@ -43,6 +66,13 @@ struct PasswordsView: View {
         credentials.remove(atOffsets: offsets)
         KeychainManager.shared.saveCredentials(credentials)
     }
+}
+
+private var presentingSheet: SheetSet = .showSettings
+private var tokenIndex: Int = 0
+
+private enum SheetSet {
+        case showSettings
 }
 
 #Preview {
