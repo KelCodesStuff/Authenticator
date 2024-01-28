cba44c71097ea560b404e89441401287c463e503
KelCodesStuff <codesmithk@gmail.com>
2024-01-27 22:31:19 -0500

    Create PasswordCardView.swift

---
+++ Authenticator/View/Passwords/PasswordCardView.swift
@@ -0,0 +1,29 @@
+//
+//  PasswordCardView.swift
+//  Authenticator
+//
+//  Created by Kelvin Reid on 1/27/24.
+//  Copyright © 2024 Studio757 LLC. All rights reserved.
+//
+
+import SwiftUI
+import Foundation
+
+struct PasswordCardView: View {
+    var credential: Credential
+
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(credential.service).font(.headline)
+                Text(credential.username).font(.subheadline)
+            }
+            Spacer()
+            Button(action: {
+                UIPasteboard.general.string = credential.password
+            }) {
+                Image(systemName: "doc.on.clipboard")
+            }
+        }
+    }
+}
