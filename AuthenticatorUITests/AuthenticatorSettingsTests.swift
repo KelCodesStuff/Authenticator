4bcde9965c4d189285fef4b7bcf21c4c31677dee
KelCodesStuff <codesmithk@gmail.com>
2025-03-24 16:36:09 -0400

    Update AuthenticatorSettingsTests.swift

--- AuthenticatorUITests/AuthenticatorSettingsTests.swift
+++ AuthenticatorUITests/AuthenticatorSettingsTests.swift
@@ -2,7 +2,7 @@
 //  AuthenticatorSettingsTests.swift
 //  Authenticator
 //
-//  Created by Kelvin Reid on 3/20/25.
+//  Created by Kel Reid on 3/20/25.
 //  Copyright © 2025 OneVR LLC. All rights reserved.
 //
 
